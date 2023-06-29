14618859f69066953df36075e65ed4caafeab44c
Nazar <Timo_qm01@student.itstep.org>
2023-06-29 23:01:11 +0300

    Fix UserId

---
+++ ClassLibrary1/Migrations/20230629195953_FixUserId.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infrustructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId1",
+                table: "Ratings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserId1",
+                table: "Ratings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Ratings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Ratings",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserId",
+                table: "Ratings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId",
+                table: "Ratings",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId",
+                table: "Ratings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserId",
+                table: "Ratings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Ratings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Ratings",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserId1",
+                table: "Ratings",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId1",
+                table: "Ratings",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
--- Core/Entities/Rating.cs
+++ Core/Entities/Rating.cs
@@ -8,8 +8,8 @@ namespace Core.Entities
 {
     public class Rating
     {
-        public int Id{ get; set; }
-        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
         public User User { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
